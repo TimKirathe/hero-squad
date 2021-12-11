fa62e280874080d1a18a9d234d7e0fec60a4558c
timothykirathe <timothy.kirathe@student.moringaschool.com>
2021-12-11 09:06:47 +0300

    Creation of the template of the App.java file

---
+++ src/main/java/App.java
@@ -0,0 +1,15 @@
+import static spark.Spark.*;
+import java.util.ArrayList;
+import java.util.List;
+
+import java.util.HashMap;
+
+import java.util.Map;
+
+import spark.ModelAndView;
+import spark.TemplateEngine;
+
+public class App {
+    public static void main(String[] args) {
+    }
+}
