b349990198c281ae37cd23becdc6f0b04f7bba1b
timothykirathe <timothy.kirathe@student.moringaschool.com>
2021-12-12 08:57:03 +0300

    Removed the team class and team test files and created a test for the hero class in order to create a team

--- src/test/java/models/HeroTest.java
+++ src/test/java/models/HeroTest.java
@@ -1,6 +1,8 @@
 package models;
 
 import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
 import java.util.List;
 import java.util.ArrayList;
 
@@ -19,5 +21,20 @@ class HeroTest {
         Hero hero = new Hero("john", 23, "super strength", "water");
         Hero hero1 = new Hero("james", 34, "invincible", "none");
         assertEquals(2, Hero.getAll().size());
+    }
+
+    @Test
+    void teamOfHerosGetsCreatedAndReturned() {
+        Hero hero1 = new Hero("john", 23, "super strength", "water");
+        Hero hero2 = new Hero("james", 34, "invincible", "none");
+        Hero hero3 = new Hero("rodgers", 13, "Control the weather", "Outer space");
+        Hero hero4 = new Hero("Sean", 45, "Heat vision", "extreme heat");
+        List<Hero> team = Arrays.asList(new Hero[4]);
+        team.set(0, hero1);
+        team.set(1, hero2);
+        team.set(2, hero3);
+        team.set(3, hero4);
+        assertEquals(true, team.size() == 4);
+
     }
 }
