1cd82deb7ff3640969918084abdf876a2b01e71d
timothykirathe <timothy.kirathe@student.moringaschool.com>
2021-12-12 11:19:15 +0300

    Created name and fighting cause properties in the Team class

--- src/main/java/models/Team.java
+++ src/main/java/models/Team.java
@@ -6,8 +6,12 @@ import java.util.List;
 
 public class Team {
     private List<Hero> team = Arrays.asList(new Hero[4]);
+    private String name;
+    private String cause;
 
-    public Team(Hero hero1, Hero hero2, Hero hero3, Hero hero4) {
+    public Team(String name, String cause, Hero hero1, Hero hero2, Hero hero3, Hero hero4) {
+        this.name = name;
+        this.cause = cause;
         team.set(0, hero1);
         team.set(1, hero2);
         team.set(2, hero3);
@@ -16,5 +20,13 @@ public class Team {
 
     public List<Hero> getTeam() {
         return team;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getCause() {
+        return cause;
     }
 }
--- src/test/java/models/TeamTest.java
+++ src/test/java/models/TeamTest.java
@@ -12,7 +12,7 @@ class TeamTest {
         Hero hero2 = new Hero("james", 34, "invincible", "none");
         Hero hero3 = new Hero("rodgers", 13, "Control the weather", "Outer space");
         Hero hero4 = new Hero("Sean", 45, "Heat vision", "extreme heat");
-        Team team = new Team(hero1, hero2, hero3, hero4);
+        Team team = new Team("Supa Strikers", "Villains", hero1, hero2, hero3, hero4);
         assertTrue(team.getTeam().size() == 4);
     }
 }
